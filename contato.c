67afb0777bf616a2c401c4adf500d6f0a03b4978
Gabriel Chiele <gabriel-chiele@cpca.pucrs.br>
2016-05-21 23:28:53 -0300

    listmsgs added

--- contato.c
+++ contato.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "contato.h"
 
@@ -17,26 +18,27 @@ int _createContato(struct contato* new, char* name, char* ip){
 	return 1;
 }
 
+void _listMsgs(struct contato* contact_list, char* name){
+	int i;
+	for (i = 0; i < N_ELEMENTS(contact_list); ++i)
+	{
+		if(!strcmp(contact_list[i]._name, name)){
+			FILE* file;
+			if(file = fopen (contact_list[i].file_name, "r")){
+				char* line;
+				while(!feof(file)){
+					line = readLine(myFile);
+					printf("%s\n", line);
+				}
+			}
+		}
+	}
+}
+
 void _main(void){
 
 	struct contato teste;
 	_createContato(&teste,"carlos","10.10.10.10");
 
 	printf("%s\n%s\n%s\n",teste._name,teste._ip,teste.file_name);
-
 }
-
-/*	
-    sprintf(file_name, "Messages/%s.msg", new->_name);
-	if(new->msgs = fopen (file_name, "r+")){
-		fseek(new->msgs,0,SEEK_END);
-		if(feof(new->msgs)){
-			printf("!!!!!!");	
-			fprintf(new->msgs,"!!");
-			}
-	}
-	else{
-		system("mkdir Messages");	
-		new->msgs = fopen (file_name, "a+");
-	}
-*/
--- contato.h
+++ contato.h
@@ -1,13 +1,16 @@
 #ifndef CONTATO_HEADER
 #define CONTATO_HEADER
 
+#define N_ELEMENTS(c)  (sizeof(c) / sizeof((c)[0]))
+
 typedef struct contato{
 	char* _name;
 	char* _ip;
 	char* file_name;
-	FILE* msgs;
 }contato;
 
 int _createContato(struct contato* new, char* name, char* ip);
+
+void _listMsgs(struct contato* contact_list, char* name);
 
 #endif
