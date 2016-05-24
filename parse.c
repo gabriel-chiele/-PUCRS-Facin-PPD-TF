bd29de74dba2b0fa236e08166ecf4d9dadfdec5a
GABRIEL CHIEZA CHIELE <13105129@l1975416.portoalegre.pucrsnet.br>
2016-05-24 18:41:49 -0300

    Add parse

---
+++ parse.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+void  readSTDIN(void){
+	char string[1024];
+	printf("\tWhatsPPD\n")
+	// lê terminal
+	while(1){
+		gets(string);
+	}
+	// criar parser para identificar comandos
+}
