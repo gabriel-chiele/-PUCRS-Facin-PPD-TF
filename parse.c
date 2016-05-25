5679f3eb335912e9e6fb130f80bc31c4ff045273
Gabriel Chiele <gabriel-chiele@cpca.pucrs.br>
2016-05-24 21:05:38 -0300

    parse

--- parse.c
+++ parse.c
@@ -1,11 +1,24 @@
-#include<stdio.h>
+#include <stdio.h>
 
 void  readSTDIN(void){
 	char string[1024];
-	printf("\tWhatsPPD\n")
-	// lê terminal
+	int i = 0, separator = 0;
 	while(1){
 		gets(string);
+		for(i=0;i<strlen(string);i++){
+			if(string[i] == NULL)
+				break;
+			else if(string[i] == '\ ')
+			{
+				printf("%s\n", string);
+				separator = i;
+			}
+			else continue;
+		}
 	}
 	// criar parser para identificar comandos
+}
+
+void main(void){
+	readSTDIN();
 }
