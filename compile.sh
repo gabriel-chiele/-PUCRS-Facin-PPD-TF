01f677f31882093a9ab40b0b6a6e215d64f4c5b2
gabriel-chiele <gabriel.chiele@cpca.pucrs.br>
2016-05-25 14:21:58 -0300

    created user login

--- compile.sh
+++ compile.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 gcc main.c contato.c mensagem.c grupo.c user.c parse.c -o main
+
+clear
--- main.c
+++ main.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 
-#include "contato.h"
-#include "mensagem.h"
-#include "grupo.h"
+#include "user.h"
+
+void closing();
+
+struct user usuario;
 
 void main(void){
+	char username[8];
 
-	struct grupo group;
+	printf("\t\t\tWhatsPPD\nUsuario:");
+	gets(username);
+
+	printf("\n%s\n", username);
+
+	/*struct grupo group;
 	struct contato contatos[8];
 	
 	_createContato(&contatos[0],"carlos","10.10.10.10");
@@ -15,6 +23,12 @@ void main(void){
 
 	_createGrupo(&group,"teste",&(contatos[0]),1); 
 
-	printf("Grupo: \n%s\n%s\n",group._name, group.contatos[0]._name);
+	printf("Grupo: \n%s\n%s\n",group._name, group.contatos[0]._name);*/
 
+	atexit(closing);
+
+}
+
+void closing(){
+	system("clear");
 }
